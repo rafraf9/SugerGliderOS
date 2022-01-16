85656f3634f76a4929f696ee0a7a3f4a938dbc85
rafraf9 <rephael.fischer@gamil.com>
2022-01-16 20:02:13 +0200

    Cleaned up workspace

--- loader.asm
+++ loader.asm
@@ -3,6 +3,7 @@ global loader   ;the entry symbol for the elf
 MAGIC_NUMBER equ 0x1BADB002 ;define the magic number
 FLAGS equ 0x0               ;multiboot flags
 CHECKSUM equ -MAGIC_NUMBER  ;(magic_number + flags + checksum) should equal zero
+KERNEL_STACK_SIZE equ 4096  ;size of stack in bytes
 
 section .text:              ;start of the text code section
 align 4                     ;the code must be 4 byte aligned
@@ -11,6 +12,11 @@ align 4                     ;the code must be 4 byte aligned
     dd CHECKSUM
 
 loader:                     ;the loader label, defined in the linker script
-    mov eax, 0xcafebabe     ;place the number in register eax
+    mov esp, kernel_stack + KERNEL_STACK_SIZE   ;point esp to the end of the stack
 .loop:
     jmp .loop               ;loop forever
+
+section .bss
+align 4
+kernel_stack:
+    resb KERNEL_STACK_SIZE      ;reserve stack for the kernel
